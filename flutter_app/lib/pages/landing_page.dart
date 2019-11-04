e90ac662c856ec340cd64e0ed0dd4bb2b24eb944
simonpiazolo <simonpiazolo@gmail.com>
2019-11-04 15:56:28 +0100

    Flutter app blue screen

---
+++ flutter_app/lib/pages/landing_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return new Material(
+      color: Colors.blueAccent,
+    );
+  }
+}
